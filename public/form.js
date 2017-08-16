03ea8b2ab9a41cc56bb25d68c8e7eafcdb1249d0
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-16 16:59:31 +0100

    Adds js to show html form

---
+++ public/form.js
@@ -0,0 +1,8 @@
+function showForm() {
+  var noteForm = document.getElementById('note-form')
+  if (noteForm.style.visibility === 'hidden') {
+    noteForm.style.visibility = 'visible';
+  } else {
+    noteForm.style.visibility = 'hidden';
+  }
+};
