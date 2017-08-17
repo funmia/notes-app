e6f229b7b7f8e70b2b0edc3ca2a5098d389b790e
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-17 17:11:02 +0100

    updating html and refactored new-note-view

--- src/form-view.js
+++
@@ -1,9 +0,0 @@
-function showForm() {
-  var noteForm = document.getElementById('note-form');
-  if (noteForm.style.visibility == 'hidden') {
-    noteForm.style.visibility = 'visible';
-  }
-  else {
-    noteForm.style.visibility = 'hidden';
-  }
-}
