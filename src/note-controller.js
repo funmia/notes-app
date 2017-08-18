c744a34d9ec10cb4ceaab49b759dd68ae14c1bd9
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-08-18 13:10:56 +0100

    updates note controller to create a note

--- src/note-controller.js
+++ src/note-controller.js
@@ -2,18 +2,30 @@
   function NoteController(noteManagerInstance) {
     this._noteManager = new noteManagerInstance();
   }
+var noteControl = new NoteController(NoteManager);
+var textAreaContent;
 
   NoteController.prototype = {
+    createNote: function(textAreaContent) {
+        noteControl._noteManager.createNote(textAreaContent);
+        console.log(noteControl._noteManager.noteList());
+    },
+
     displayList: function() {
       var noteList = this._noteManager.noteList();
       var noteListTitles = [];
-      console.log(noteList);
       noteList.forEach(function(element){
         noteListTitles.push(element._title);
       });
     return noteListView.toHtml(noteListTitles);
     }
   };
+
+  document.getElementById('note-form').addEventListener("submit", function(event) {
+    event.preventDefault();
+    textAreaContent = event.target.elements["0"].value;
+    NoteController.prototype.createNote(textAreaContent)
+  });
 
   exports.NoteController = NoteController;
 })(this);
